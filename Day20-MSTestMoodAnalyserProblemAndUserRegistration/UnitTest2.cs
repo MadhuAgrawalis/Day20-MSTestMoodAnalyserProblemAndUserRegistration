2bf87d272e500cece6fb238ce30d8d27e4958439
MadhuAgrawalis <madhubalasagrawal56@gmail.com>
2022-02-14 11:35:34 +0530

    Uc2

---
+++ Day20-MSTestMoodAnalyserProblemAndUserRegistration/UnitTest2.cs
@@ -0,0 +1,38 @@
+﻿using TestProject1;
+
+namespace TestProject2
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        public object Assert { get; private set; }
+
+        //MoodAnalyser moodAnalyser;
+        //string message = " I am in happy Mood";
+        [TestInitialize]
+        public void SetUp()
+        {
+            //moodAnalyser = new MoodAnalyser(message);
+        }
+        [TestMethod]
+        public void TestMethodForHappyMood()
+
+        {
+            string expected = "happy";
+            MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy Mood");
+            string actual = moodAnalyser.AnalyzeMood();
+
+            object p = Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void TestMethodForSadMood()
+
+        {
+            string expected = "sad";
+            MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad Mood");
+            string actual = moodAnalyser.AnalyzeMood();
+            object p = Assert.AreEqual(expected, actual);
+        }
+    }
+}
